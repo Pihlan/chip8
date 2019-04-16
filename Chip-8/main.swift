c3e7f6c775d250192c668ae6e72313941681e02e
Fredrik Pihlström <fredrik@relieftryck.se>
2019-04-16 16:05:03 +0200

    Initial Commit

---
+++ Chip-8/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Chip-8
+//
+//  Created by Fredrik Pihlström on 2019-04-16.
+//  Copyright © 2019 Göteborgs Relief. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
